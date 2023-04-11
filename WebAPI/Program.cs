69674993b5868946ecd6d3925e14f3f87f33d8a1
Daniel Chang <daniel.chang@cloudnifier.com>
2023-04-12 00:09:35 +0800

    設定Configure和Serilog

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,25 +1,55 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    #region Configure
+    builder.Host.ConfigureAppConfiguration((hostContext, config) =>
+    {
+        var env = hostContext.HostingEnvironment;
+        config.SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+          .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true);
+    })
+        .UseSerilog((hostingContext, loggerConfig) =>
+         loggerConfig.ReadFrom.Configuration(hostingContext.Configuration).Enrich.FromLogContext()
+         );
+
+    var config = builder.Configuration;
+
+    Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+    #endregion
+
+    #region DB
+
+    #endregion
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception)
+{
+    Console.WriteLine("Error");
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
