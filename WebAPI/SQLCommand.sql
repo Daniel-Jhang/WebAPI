712252fb567fb3404174d7c23f2b9bcec57fe887
Ethan Chang (張逸鴻) <ethanchang@cybersoft.com.tw>
2023-08-18 17:30:55 +0800

    加入文件

---
+++ WebAPI/SQLCommand.sql
@@ -0,0 +1,13 @@
+CREATE DataBase LAB;
+
+USE [LAB]
+GO
+
+CREATE TABLE [LAB].[dbo].[TodoList](
+	[sqlId] [int] IDENTITY(1,1),
+	[TodoId] [uniqueidentifier] PRIMARY KEY,
+	[Status] [bit] NOT NULL,
+	[Editing] [bit] NOT NULL,
+	[Context] [varchar](255) NOT NULL,
+	)
+GO
