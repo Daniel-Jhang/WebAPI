43292adbe2c8e0a34f41f3f021a7d2595b7b5d7c
逸鴻 張 <winterer32@gmail.com>
2023-08-23 22:17:50 +0800

    新增LabContext, Models

---
+++ DataAccessLibrary/Models/TodoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLibrary.Models;
+
+public partial class TodoList
+{
+    public int SqlId { get; set; }
+
+    public Guid TodoId { get; set; }
+
+    public bool Status { get; set; }
+
+    public bool Editing { get; set; }
+
+    public string Context { get; set; } = null!;
+}
